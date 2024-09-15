b58b4b616ddde2771266f9f98a6efd36b4a60f32
Alezer0Dev <provaadobe@proton.me>
2024-09-15 19:12:58 +0200

    Aggiungere i file di progetto.

---
+++ Bank Simulator/BankAccount.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Simulator
+{
+    public class BankAccount {
+        private string name;
+        private int balance;
+        private string iban;
+        private List<string> history = new List<string>();
+        static List<BankAccount> accounts = new List<BankAccount>();
+        static BankAccount currentAccount;
+
+        BankAccount(string name, int balance, string iban) {
+            this.name = name;
+            this.balance = balance;
+            this.iban = iban;
+        }
+
+        public static void NewAccount(string name, int balance, string iban) {
+            Console.WriteLine("Creazione account..");
+            accounts.Add(new BankAccount(name, balance, iban));
+            Console.WriteLine("Account creato");
+            Console.WriteLine("Accesso in corso..");
+            LogIn(name);
+        }
+
+        public static void LogIn(string name) {
+            currentAccount = FindAccountByName(name);
+            Console.WriteLine("Accesso completato con successo");
+        }
+
+        static BankAccount FindAccountByName (string name) {
+            if (accounts.Count == 0) throw new Exception("Non esiste nessun account");
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (name == accounts[i].name)
+                {
+                    return accounts[i];
+                }
+            }
+            throw new Exception("Nessun account esistente con questo nome");
+        }
+
+        public static bool IsIbanUsed(string iban) {
+            if (accounts.Count == 0) return false;
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (iban == accounts[i].iban)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsNameUsed(string name)
+        {
+            if (accounts.Count == 0) return false;
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (name == accounts[i].name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void Deposit(int amount) {
+            currentAccount.balance += amount;
+            currentAccount.history.Add($"+{amount}€");
+        }
+
+        public static void Withdraw(int amount) {
+            if (currentAccount.balance >= amount)
+            {
+                currentAccount.balance -= amount;
+                currentAccount.history.Add($"-{amount}€");
+            }
+            else { Console.WriteLine("Non hai abbastanza soldi"); }
+        }
+
+        public static int GetBalance() { return currentAccount.balance; }
+
+        public static void PrintHistory() {
+            Console.WriteLine("\nCronologia:");
+            for (int i = 0; i < currentAccount.history.Count; i++) {
+                Console.WriteLine(currentAccount.history[i]);
+            }
+        }
+
+        public static void LogOut() {
+            Console.WriteLine("Sto uscendo dall'account..");
+            currentAccount = null;
+            Console.WriteLine("Logout effettuato con successo");
+        }
+    }
+}
---
+++ Bank Simulator/Program.cs
@@ -0,0 +1,78 @@
+﻿using Bank_Simulator;
+using System.Xml.Serialization;
+
+string name;
+int bonus = 1000;
+int balance = 0;
+string iban;
+int choice;
+int amount;
+Random rand = new Random();
+
+Console.WriteLine("Inserisci il tuo nome: ");
+
+do {
+    name = Console.ReadLine();
+}
+while (String.IsNullOrWhiteSpace(name) || BankAccount.IsNameUsed(name));
+
+balance += bonus;
+
+do {
+    iban = "IT";
+    iban = iban += rand.Next(1000000000, 1999999999);
+} while (BankAccount.IsIbanUsed(iban));
+
+BankAccount.NewAccount(name, balance, iban);
+Console.WriteLine($"Conto creato con i seguenti dati:\nNome titolare: {name}\nSaldo: {balance}\nIBAN: {iban}");
+do
+{
+    Console.WriteLine("Cosa vuoi fare?\n1) Deposita\n2) Preleva\n3) Visualizza saldo\n4) Visualizza cronologia delle operazioni\n5) Logout");
+
+    do {
+        choice = Convert.ToInt32(Console.ReadLine());
+    }
+    while (choice < 1 || choice > 5);
+
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Quanti soldi vuoi depositare?:");
+            do
+            {
+                amount = Convert.ToInt32(Console.ReadLine());
+            }
+            while (amount <= 0);
+            BankAccount.Deposit(amount);
+            amount = 0;
+            break;
+
+        case 2:
+            Console.WriteLine("Quanti soldi vuoi prelevare?:");
+            do
+            {
+                amount = Convert.ToInt32(Console.ReadLine());
+            }
+            while (amount <= 0);
+            BankAccount.Withdraw(amount);
+            amount = 0;
+            break;
+
+        case 3:
+            Console.WriteLine($"Il tuo saldo è €{BankAccount.GetBalance()}");
+            break;
+
+        case 4:
+            BankAccount.PrintHistory();
+            break;
+
+        case 5:
+            BankAccount.LogOut();
+            break;
+
+        default:
+            Console.WriteLine("Scelta non valida");
+            break;
+    }
+}
+while (choice != 5);
